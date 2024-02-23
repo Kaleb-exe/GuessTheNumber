bb57c9bd801c9cc9ed52e3cd2496c0d1cf5fc6ba
Kaleb-exe <64001050@ep-student.org>
2024-02-23 11:11:53 -0600

    Initial Commit

---
+++ GuessTheNumber/ContentView.swift
@@ -0,0 +1,57 @@
+//
+//  ContentView.swift
+//  GuessTheNumber
+//
+//  Created by 64001050 on 2/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var guess = ""
+    @State var feedback = ""
+    let targetNumber = Int.random(in: 1...100)
+    @State var gameEnded = false
+
+    var body: some View {
+        VStack {
+            Text("Welcome to Guess the Number!")
+            Text("I've selected a number between 1 and 100. Can you guess what it is?")
+            TextField("Enter your guess", text: $guess)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+                .keyboardType(.numberPad)
+            Button("Submit Guess") {
+                guard let guessInt = Int(guess) else {
+                    feedback = "Invalid input. Please enter a valid number."
+                    return
+                }
+
+                if guessInt == targetNumber {
+                    gameEnded = true
+                } else {
+                    feedback = "Sorry, that's not the correct number. Try again!"
+                }
+            }
+            .padding()
+            Text(feedback)
+        }
+        .padding()
+        .alert(isPresented: $gameEnded) {
+            Alert(title: Text("Congratulations!"),
+                  message: Text("You guessed the correct number!"),
+                  dismissButton: .default(Text("OK")))
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
+#Preview {
+    ContentView()
+}
---
+++ GuessTheNumber/GuessTheNumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheNumberApp.swift
+//  GuessTheNumber
+//
+//  Created by 64001050 on 2/21/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheNumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
